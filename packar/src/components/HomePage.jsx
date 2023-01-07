24a98442d4ceee1cc43dbfbb25dbb26be73ca83a
Marta Salvador <martasbsv@gmail.com>
2023-01-07 15:13:14 +0000

    fizzBuzz

--- packar/src/components/HomePage.jsx
+++ packar/src/components/HomePage.jsx
@@ -5,26 +5,40 @@ import './HomePage.css'
 
 const HomePage = () => {
 
- const [cat, setCat] = useState([])
+    const [cat, setCat] = useState([])
+    const [data, setData] = useState()
+    
 
      const loadData = () => {
 
         apiService
             .getOneCat()
-            .then(({ cat }) => setCat(cat))
-            console.log('entro aqui')
-            console.log(cat)
+            .then((cat) => setCat(cat))
+                console.log('Estoy en el servicio de la API')
+                console.log(cat.data[0].url)
             .catch(err => console.log(err))
      }
     
     const handleData = () => [
-        loadData()
+        loadData(),
+        setData(cat.data[0].url)
     ]
+    useEffect(() => {
+   
+    },[handleData])
     
     return (
         <Container className="home">
-            <h1 className="title">Random Cat</h1>
-            <Button className="newCatBtn" onClick={() => handleData()}>New Cat</Button>
+            <h1 className="title">Random Cats</h1>
+            <Button className="newCatBtn btn-warning" onClick={() => handleData()}>New Cat</Button>
+            <br></br>
+            {
+                data ?
+                    <img src={cat.data[0].url} alt="cat image" className="image" />
+                    :
+                    <p>Searching...</p>
+             }
+            
         </Container>
     )
 }
